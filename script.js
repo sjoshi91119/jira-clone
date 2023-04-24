1ad79da821d421c8274fa394131d510542fb1468
Shubham Joshi <sjoshi91119@gmail.com>
2023-04-24 15:28:11 +0530

    made changes to ui

---
+++ script.js
@@ -0,0 +1,54 @@
+var btn = document.getElementById("btn");
+
+function clickAction() {
+    console.log("button was clicked.")
+};
+
+// var myHeading = document.getElementById("heading2");
+
+// console.log(myHeading);
+// console.log(myHeading.parentElement.parentElement);
+// console.log(myHeading.previousElementSibling);
+// console.log(myHeading.nextElementSibling);
+
+// .cssText method is also useful
+
+ 
+//======================================================
+
+// how to make a button make things visible on click and invisible when clicked again.
+
+//  var open  = false;
+
+//  function changeStyles(){
+
+//     if(!open){
+//         open = true;
+//         document.getElementById("hamburger").style.display = "block";
+//     }
+//     else{
+//         open = false;
+//         document.getElementById("hamburger").style.display = "none";
+//     }
+//  }
+
+//=======================================================
+
+
+// taking out value from an input field (#my email is the input id)
+
+
+// function submitEmail(){
+//     var myInput = document.getElementById("my-email");
+
+//     var userEmail - myInput.value;
+//     console.log(userEmail);
+// }
+
+
+
+//===========================================================
+
+
+
+
